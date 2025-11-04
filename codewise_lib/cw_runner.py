f1d1bb9b7fddb608ed8fa7ad6d378d8736678ccd
Matheus Magnago <magnagomatheus7@gmail.com>
2025-11-03 21:46:57 -0300

    Adding verification to see with already exists a LGPD analysics from the same provider and model

--- codewise_lib/cw_runner.py
+++ codewise_lib/cw_runner.py
@@ -12,90 +12,19 @@ class CodewiseRunner:
         self.caminho_entrada = os.path.join(self.BASE_DIR, ".entrada_temp.txt")
 
     def executar(self, caminho_repo: str, nome_branch: str, modo: str):
-        
-        # Tentativa de colocar a analise lgpd para rodar antes do envio dos dados sensiveis
-        # instancia sem passar o commit como contexto
-        codewise_instance = Codewise()
 
-        print(f"Verificando a política de coleta de dados do provedor com base neste modelo de api key...")
+        caminho_dir_lgpd = os.path.join(caminho_repo, "analises-julgamento-lgpd")
+        policy_file_path = os.path.join(caminho_dir_lgpd, "analise_politica_coleta_de_dados.md")
+        lgpd_judge_file_path = os.path.join(caminho_dir_lgpd, "julgamento_lgpd.md")
 
-        #lgpd crew
-        lgpd_check_crew = codewise_instance.lgpd_crew()
-
-        # roda a analise e o julgamento lgpd (todo o time)
-        lgpd_check_crew.kickoff()
-
-        #definicao do caminho
-        output_dir_name = "analises-julgamento-lgpd"
-        output_dir_path = os.path.join(caminho_repo, output_dir_name)
-        os.makedirs(output_dir_path, exist_ok=True)
-
-        # salvando o resultado da analise da politica de coleta de dados
-        resultado_policy = lgpd_check_crew.tasks[0].output
-
-        # definindo caminho e nome do arquivo final.
-        policy_file_path = os.path.join(output_dir_path, "analise_politica_coleta_de_dados.md")
-
-        try:
-            with open(policy_file_path, "w", encoding="utf-8") as f:
-                f.write(str(resultado_policy))
-                print(f"   - Arquivo 'analise_politica_coleta_de_dados.md' salvo com sucesso em '{output_dir_path}'.", file=sys.stderr)
-        except Exception as e:
-            print(f"    - ERRO ao salvar o arquivo 'analise_politica_coleta_de_dados.md': {e}", file=sys.stderr)
-        
-        
-        #salvando o julgamento lgpd
-        resultado_lgpd = lgpd_check_crew.tasks[1].output
-
-        # definindo caminho e nome do arquivo final.
-        lgpd_judge_file_path = os.path.join(output_dir_path, "julgamento_lgpd.md")
-
-        try:
-            with open(lgpd_judge_file_path, "w", encoding="utf-8") as fj:
-                fj.write(str(resultado_lgpd))
-                print(f"    - Arquivo 'julgamento_lgpd.md' salvo com sucesso em '{output_dir_path}'.", file=sys.stderr)
-        except Exception as e:
-            print(f"    - ERRO ao salvar o arquivo 'julgamento_lgpd.md': {e}", file=sys.stderr)
-        
-        # fim
-
-        # Verificação dos resultados da política de coleta de dados do provedor e model utilizados
-        caminho = os.path.join("analises-julgamento-lgpd", "julgamento_lgpd.md")
         status = False
-        print("****************")
-        print("Arquivo existe?", os.path.exists(policy_file_path))
-        print("Arquivo existe (status)?", os.path.exists(lgpd_judge_file_path))
-        print("****************")
-        try:
-            with open(lgpd_judge_file_path, "r", encoding="utf-8") as julgamento:
-                #print(julgamento.readline())
-
-                for linha in julgamento:
-                    linha_clean = linha.strip().lower()
-
-                    linha_clean = re.sub(r'[*_#>`~]', '', linha_clean).strip()
-
-                    linha_clean = linha_clean.strip()
-                    
-                    print(f"{linha_clean}")
-                    if(linha_clean == "sim"):
-                        print(f"SSSSSSSIIIMMMMMMMMMFASOKFSAOFKAOFKAOS")
-                        status = True
-                        print("Provedor requisitado está respeitando as normas LGPD, podemos proseguir com a análise.")
-                        print(f"Acesse os arquivos 'analise_politica_coleta_de_dados.md.' e 'julgamento_lgpd.md' para comprovações do julgamento.")
-                        break
-                    if (linha_clean == "não"):
-                        status = False
-                        print(f"NAAAAAAOFASOKFSAOFKAOFKAOS")
-                        print("Provedor requisitado não está dentro das normas LGPD! Por conta disso, as análises foram interrompidas antes que seus dados fossem enviados.")
-                        print(f"Tente novamente escolhendo outro provedor ou modelo de api key.")
-                        print(f"Para mais informações, acesse os arquivos 'analise_politica_coleta_de_dados.md.' e 'julgamento_lgpd.md'.")
-                        break
-                    print(f"{linha}")
-        except Exception as e:
-            print(f"    - ERRO ao ler o arquivo 'julgamento_lgpd.md': {e}", file=sys.stderr)
         
-        print(status)
+        if(self.verifica_se_existe_analise_lgpd(policy_file_path, lgpd_judge_file_path)):
+            print("Verificando o julgamento da análise existente...")
+            status = self.verify_result_judgement(lgpd_judge_file_path)
+        else:
+            print("Iniciando análise e julgamento LGPD...")
+            status = self.verify_lgpd(caminho_repo, caminho_dir_lgpd, policy_file_path, lgpd_judge_file_path)
         
         if(status == True):
 
@@ -210,3 +139,99 @@ class CodewiseRunner:
         try:
             with open(file_path, "r", encoding="utf-8") as f: return f.read()
         except FileNotFoundError: return ""
+
+    def verify_lgpd(self, caminho_repo: str, caminho_dir_lgpd: str, policy_file_path: str, lgpd_judge_file_path: str) -> bool:
+        # Tentativa de colocar a analise lgpd para rodar antes do envio dos dados sensiveis
+        # instancia sem passar o commit como contexto
+        codewise_instance = Codewise()
+
+        print(f"Verificando a política de coleta de dados do provedor com base neste modelo de api key...")
+
+        #lgpd crew
+        lgpd_check_crew = codewise_instance.lgpd_crew()
+
+        # roda a analise e o julgamento lgpd (todo o time)
+        lgpd_check_crew.kickoff()
+
+        #definicao do caminho --> criacao directory
+        os.makedirs(caminho_dir_lgpd, exist_ok=True)
+
+        # salvando o resultado da analise da politica de coleta de dados
+        resultado_policy = lgpd_check_crew.tasks[0].output
+
+        # definindo caminho e nome do arquivo final.
+        #policy_file_path = os.path.join(output_dir_path, "analise_politica_coleta_de_dados.md")
+
+        try:
+            with open(policy_file_path, "w", encoding="utf-8") as f:
+                f.write(str(resultado_policy))
+                print(f"   - Arquivo 'analise_politica_coleta_de_dados.md' salvo com sucesso em '{caminho_dir_lgpd}'.", file=sys.stderr)
+        except Exception as e:
+            print(f"    - ERRO ao salvar o arquivo 'analise_politica_coleta_de_dados.md': {e}", file=sys.stderr)
+        
+        
+        #salvando o julgamento lgpd
+        resultado_lgpd = lgpd_check_crew.tasks[1].output
+
+        # definindo caminho e nome do arquivo final.
+        #lgpd_judge_file_path = os.path.join(output_dir_path, "julgamento_lgpd.md")
+
+        try:
+            with open(lgpd_judge_file_path, "w", encoding="utf-8") as fj:
+                fj.write(str(resultado_lgpd))
+                print(f"    - Arquivo 'julgamento_lgpd.md' salvo com sucesso em '{caminho_dir_lgpd}'.", file=sys.stderr)
+        except Exception as e:
+            print(f"    - ERRO ao salvar o arquivo 'julgamento_lgpd.md': {e}", file=sys.stderr)
+        
+        # fim
+
+        # Verificação dos resultados da política de coleta de dados do provedor e model utilizados
+        return self.verify_result_judgement(lgpd_judge_file_path)
+    
+    def verify_result_judgement(self, lgpd_judge_file_path) -> bool:
+        try:
+            with open(lgpd_judge_file_path, "r", encoding="utf-8") as julgamento:
+                #print(julgamento.readline())
+
+                for linha in julgamento:
+                    linha_clean = linha.strip().lower()
+
+                    linha_clean = re.sub(r'[*_#>`~]', '', linha_clean).strip()
+
+                    linha_clean = linha_clean.strip()
+
+                    if(linha_clean == "sim"):
+                        return True
+                    if (linha_clean == "não"):
+                        return False
+        except Exception as e:
+            print(f"    - ERRO ao ler o arquivo 'julgamento_lgpd.md': {e}", file=sys.stderr)
+    
+    def verifica_se_existe_analise_lgpd(self, policy_file_path, lgpd_judge_file_path) -> bool:
+
+        provider = os.getenv("AI_PROVIDER").lower()
+        model = os.getenv("AI_MODEL").lower()
+        model = re.sub(r'[*_#>`~]', '', model)
+
+        provider_model = provider + model
+
+        if(os.path.exists(policy_file_path) and os.path.exists(lgpd_judge_file_path)):
+            try:
+                with open(policy_file_path, "r", encoding="utf-8") as f:
+                    #print(julgamento.readline())
+
+                    for linha in f:
+                        linha_clean = linha.strip().lower()
+
+                        linha_clean = re.sub(r'[*_#>`~]', '', linha_clean).strip()
+
+                        linha_clean = linha_clean.strip()
+                        if(linha_clean == provider_model):
+                            print("A análise e verificação LGPD já foi feita anteriormente para este mesmo provedor e modelo api key. Por conta disso, não será necessário efetuá-la novamente!")
+                            return True
+                        if(not linha):
+                            return False
+            except Exception as e:
+                print(f"    - ERRO ao ler o arquivo 'analise_politica_coleta_de_dados.md': {e}", file=sys.stderr)    
+        else:
+            return False
